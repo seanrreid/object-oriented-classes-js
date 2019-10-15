2134a22be3abab19916255421750153ba3b95673
Sean R Reid <sean@torchcodelab.com>
2019-10-15 10:23:55 -0400

    Updated code to starter version

--- index.js
+++ index.js
@@ -4,18 +4,9 @@ Create a class called Person which accepts the name of a person as a string, and
 The Person class should have a method called describe which returns a string with the following syntax: "name, age years old". So for example, if John is 19 years old then the function describe of his object will return "John, 19 years old".
 */
 
-class Person { 
-    constructor (name, age) {
-        this.name = name; 
-        this.age = age;
-    };
+class Person {}
 
-    describe = subject => {
-        return `${this.name}, ${this.age} years old.`;
-    }
-}; 
-
-const me = new Person('Sean', 44);
+const me = new Person("Sean", 44);
 
 console.log(me.describe());
 
@@ -24,31 +15,8 @@ Create an object called Teacher derived from the Person class, and implement a m
 */
 
 (function() {
-    const Person = function(){};
-    const Teacher = function(){};
-    
-    Person.prototype.initialize = name => {
-        this.name = name;
-    }
-
-    Teacher.prototype = new Person();
-    Teacher.prototype.teach = subject => {
-        return `${this.name} teaches ${subject}`;
-    }
-
-    const meTeacher = new Teacher();
-    meTeacher.initialize('Sean');
-    console.log(meTeacher.teach('webdev with prototypes'))
-
+    const Person = function() {};
+    const Teacher = function() {};
 })();
 
-(function(){
-    class Teacher extends Person {
-        teach = subject => {
-            return `${this.name} teaches ${subject}`;
-        }
-    }
-
-    const meTeacher = new Teacher('Sean');
-    console.log(meTeacher.teach('webdev with classes'));
-})();
+(function() {})();
