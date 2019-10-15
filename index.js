3d1e48fd354cf05d8cdf5fe24e67288a8e7a58f5
Sean R Reid <sean@torchcodelab.com>
2019-10-15 10:36:23 -0400

    Step 1

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 /*
 Create a class called Person which accepts the name of a person as a string, and his/her age as a number.
 
-The Person class should have a method called describe which returns a string with the following syntax: "name, age years old". So for example, if John is 19 years old then the function describe of his object will return "John, 19 years old".
+The Person class should have a method called `describe()` which returns a string with the following syntax: "name, age years old". So for example, if John is 19 years old then the function describe() of his object will return "John, 19 years old".
 */
 
-class Person {}
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    describe() 
+}
 
 const me = new Person("Sean", 44);
 
-console.log(me.describe());
+console.log(me);
+
+//console.log(me.describe());
 
 /*
 Create an object called Teacher derived from the Person class, and implement a method called teach which receives a string called subject, and prints out:
 */
 
-(function() {
-    const Person = function() {};
-    const Teacher = function() {};
-})();
+// (function() {
+//     const Person = function() {};
+//     const Teacher = function() {};
+// })();
 
-(function() {})();
+// (function() {})();
