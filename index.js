4e3ae909f4078d3935fd769639c15a57d31a539a
Sean R Reid <sean@torchcodelab.com>
2020-02-25 09:54:14 -0500

    Walkthrough adding sayHello() and describe() methods

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ Create a class called Person which accepts the name of a person as a string, and
 The Person class should have a method called describe which returns a string with the following syntax: "name, age years old". So for example, if John is 19 years old then the function describe of his object will return "John, 19 years old".
 */
 
-class Person {}
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-const me = new Person("Sean", 44);
+  sayHello() {
+    console.log(`Hi, my name is ${this.name}`);
+  }
+  describe() {
+    console.log(`${this.name}, ${this.age} years old`);
+  }
+}
 
-console.log(me.describe());
+const me = new Person('Sean', 44);
+me.sayHello();
+me.describe();
 
 /*
 Create an object called Teacher derived from the Person class, and implement a method called teach which receives a string called subject, and prints out:
 */
-
-(function() {
-    const Person = function() {};
-    const Teacher = function() {};
-})();
-
-(function() {})();
